ce2e817552eb3e068324e7956dffbff66b7ecca7
suzhijie <suzhijie@bigtreefinance.com>
2020-12-24 17:42:57 +0800

    feat: 创建通用组件

---
+++ src/hooks/Dropdown.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="dropdown">
+  <a class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">
+      {{ title }}
+  </a>
+
+  <ul class="dropdown-menu"  :style="{display: 'block'}" v-if="isOpen">
+      <slot></slot>
+  </ul>
+</div>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, onUnmounted, ref } from "vue";
+export default defineComponent({
+    name: 'Dropdown',
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    },
+    setup(){
+        const isOpen = ref(false);
+        const toggleOpen = () => {
+            isOpen.value = !isOpen.value
+        }
+        const handler = (e: MouseEvent) => {
+
+        };
+        onMounted(()=> {
+            document.addEventListener('click', handler);
+        });
+        onUnmounted(()=> {
+            document.removeEventListener('click', handler);
+        })
+    return {
+      isOpen,
+      toggleOpen
+    }
+    }
+})
+</script>
+<style>
+.dropdown-option.is-disabled * {
+  color: #6c757d;
+  pointer-events: none;
+  background-color: transparent;
+}
+</style>
---
+++ src/hooks/DropdownItem.vue
@@ -0,0 +1,17 @@
+<template>
+<li class="dropdown-option" :class="{'is-disabled': disabled}">
+    <slot></slot>
+</li>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: 'DropdownItem',
+    props: {
+        disabled: {
+            type: Boolean,
+            required: true
+        }
+    }
+})
+</script>
